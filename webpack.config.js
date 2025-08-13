49f2f25db621658af9f5cdb37802454be990a2f3
Ssejjengo Edward <137992303+SsejjengoEdward8576@users.noreply.github.com>
2025-08-13 14:45:00 +0300

    Update webpack.config.js
    Added hashed filenames

--- webpack.config.js
+++ webpack.config.js
@@ -1,65 +1,99 @@
 const path = require('path');
 const autoprefixer = require('autoprefixer');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
-module.exports = {
-  mode: 'development',
-  entry: './src/index.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
-    publicPath: '/' // Ensure assets are served from the root URL
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: 'babel-loader'
-      },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader']
-      },
-      {
-        test: /\.scss$/,
-        use: [
-          'style-loader',
-          'css-loader',
-          {
-            loader: 'postcss-loader',
-            options: {
-              postcssOptions: {
-                plugins: [
-                  autoprefixer
-                ]
-              }
-            }
-          },
-          'sass-loader'
-        ]
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource'
-      }
-    ]
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './public/index.html',
-      filename: 'index.html' // Make sure the filename matches your entry point
-    })
-  ],
-  resolve: {
-    extensions: ['.js', '.jsx']
-  },
-  devServer: {
-    static: {
-      directory: path.join(__dirname, 'public') // Serve static files from public directory
+module.exports = (env, argv) => {
+  const isProd = argv.mode === 'production';
+
+  return {
+    mode: isProd ? 'production' : 'development',
+
+    entry: './src/index.js',
+
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: isProd ? 'js/[name].[contenthash].js' : 'js/[name].js',
+      publicPath: '' // Safe for GitHub Pages when using HashRouter
     },
-    compress: true,
-    port: 9000,
-    historyApiFallback: true // Fallback to index.html for all routes
-  }
+
+    resolve: {
+      extensions: ['.js', '.jsx']
+    },
+
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: 'babel-loader'
+        },
+        {
+          test: /\.css$/,
+          use: [
+            isProd ? MiniCssExtractPlugin.loader : 'style-loader',
+            'css-loader'
+          ]
+        },
+        {
+          test: /\.scss$/,
+          use: [
+            isProd ? MiniCssExtractPlugin.loader : 'style-loader',
+            'css-loader',
+            {
+              loader: 'postcss-loader',
+              options: {
+                postcssOptions: {
+                  plugins: [autoprefixer]
+                }
+              }
+            },
+            'sass-loader'
+          ]
+        },
+        {
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          type: 'asset/resource',
+          generator: {
+            filename: 'images/[name][hash][ext][query]'
+          }
+        },
+        {
+          test: /\.(woff(2)?|eot|ttf|otf)$/i,
+          type: 'asset/resource',
+          generator: {
+            filename: 'fonts/[name][hash][ext][query]'
+          }
+        }
+      ]
+    },
+
+    plugins: [
+      new CleanWebpackPlugin(),
+      new HtmlWebpackPlugin({
+        template: './public/index.html',
+        filename: 'index.html'
+      }),
+      ...(isProd
+        ? [new MiniCssExtractPlugin({ filename: 'css/[name].[contenthash].css' })]
+        : [])
+    ],
+
+    devServer: {
+      static: {
+        directory: path.join(__dirname, 'public')
+      },
+      compress: true,
+      port: 9000,
+      historyApiFallback: true
+    },
+
+    optimization: {
+      splitChunks: {
+        chunks: 'all'
+      },
+      runtimeChunk: 'single'
+    }
+  };
 };
